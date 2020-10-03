d9157a43cd58340d52d18546f8767d1ca2dc036c
Guillermo Angulo <anyermo@gmail.com>
2020-10-03 16:01:47 -0300

    Create Ribbon component (with test) and integration with Banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -32,4 +32,31 @@ describe('<Banner />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render a Ribbon with normal size and primary color by default', () => {
+    renderWithTheme(<Banner {...props} ribbon="My ribbon" />)
+
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#F231A5' })
+    expect(ribbon).toHaveStyle({ height: '3.6rem', fontSize: '1.4rem' })
+  })
+
+  it('should render a Ribbon with small size and secondary color', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({ height: '2.6rem', fontSize: '1.2rem' })
+  })
 })
