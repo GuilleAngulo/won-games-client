7fb5554d289a8665f7459ae888431e1047b13e8a
Guillermo Angulo <anyermo@gmail.com>
2020-10-05 17:49:05 -0300

    Create GameCard component and tests

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,65 @@
+import {
+  AddShoppingCart,
+  Favorite,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+import Button from 'components/Button'
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
+import * as S from './styles'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+  promotionalPrice?: string
+  favorite?: boolean
+  onFav?: () => void
+  ribbon?: string
+  ribbonSize?: RibbonSizes
+  ribbonColor?: RibbonColors
+}
+
+const GameCard = ({
+  title,
+  developer,
+  img,
+  price,
+  promotionalPrice,
+  favorite = false,
+  onFav,
+  ribbon,
+  ribbonSize = 'small',
+  ribbonColor = 'primary'
+}: GameCardProps) => (
+  <S.Wrapper>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+    {!!ribbon && (
+      <Ribbon size={ribbonSize} color={ribbonColor}>
+        {ribbon}
+      </Ribbon>
+    )}
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton onClick={onFav} role="button">
+        {favorite ? (
+          <Favorite aria-label="Remove from Wishlist" />
+        ) : (
+          <FavoriteBorder aria-label="Add to Wishlist" />
+        )}
+      </S.FavButton>
+      <S.BuyBox>
+        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+        <S.Price>{promotionalPrice || price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default GameCard
