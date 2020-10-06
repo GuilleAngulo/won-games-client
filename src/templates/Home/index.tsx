c06e7dec230900fea24b7676e386088b42f4ef86
Guillermo Angulo <anyermo@gmail.com>
2020-10-06 19:06:22 -0300

    Create Home template page

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+import Heading from 'components/Heading'
+import Menu from 'components/Menu'
+
+import * as S from './styles'
+
+const Home = () => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        News
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        Upcoming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        Free Games
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Most Popular
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
+)
+
+export default Home
