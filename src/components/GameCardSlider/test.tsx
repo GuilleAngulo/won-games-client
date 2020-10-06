08f66f24c6a0fd75a8dfb4a3835ad5094e978077
Guillermo Angulo <anyermo@gmail.com>
2020-10-06 13:26:22 -0300

    Create Container component and test

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,33 @@
+import 'match-media-mock'
+import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { items } from './stories'
+
+import GameCardSlider from '.'
+
+describe('<GameCardSlider />', () => {
+  it('should render six slides', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(6)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#030517'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render white arrows when color is passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})
