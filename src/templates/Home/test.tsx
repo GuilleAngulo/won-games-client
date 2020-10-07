3fcfe2172ce3fb46350e65006f2c38e19f24475a
Guillermo Angulo <anyermo@gmail.com>
2020-10-07 17:09:34 -0300

    Optimize Home test performance

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,13 +10,13 @@ import Home from '.'
 
 const props = {
   banners: bannersMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightsMock,
-  mostPopularGames: gamesMock,
-  upcomingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcomingGames: [gamesMock[0]],
   upcomingHighlight: highlightsMock,
-  upcomingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcomingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighlight: highlightsMock
 }
 
@@ -25,15 +25,12 @@ describe('<Home />', () => {
     renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument()
 
     expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
-  })
-
-  it('should render the sections', () => {
-    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
@@ -45,15 +42,11 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /most popular/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render section elements', () => {
-    renderWithTheme(<Home {...props} />)
-    // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
-    // highlight
-    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
+
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+
+    expect(screen.getAllByText(/read dead it´s back!/i)).toHaveLength(3)
   })
 })
