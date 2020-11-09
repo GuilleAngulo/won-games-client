e34c428f40f52caaf2ee2c8cb9e17ad47046580e
Guillermo Angulo <anyermo@gmail.com>
2020-11-09 10:56:49 -0300

    Refactor: Add generic Form component

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 import { Email, Lock } from '@styled-icons/material-outlined'
+
 import Button from 'components/Button'
 import TextField from 'components/TextField'
+
 import * as S from './styles'
+import { FormLink, FormWrapper } from 'components/Form'
 
 const FormSignIn = () => (
-  <S.Wrapper>
+  <FormWrapper>
     <form>
       <TextField
         name="email"
@@ -25,14 +28,14 @@ const FormSignIn = () => (
         Sign in now
       </Button>
 
-      <S.FormLink>
+      <FormLink>
         Don`t have an account?{' '}
         <Link href="sign-up">
           <a>Sign up</a>
         </Link>
-      </S.FormLink>
+      </FormLink>
     </form>
-  </S.Wrapper>
+  </FormWrapper>
 )
 
 export default FormSignIn
