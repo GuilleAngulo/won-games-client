974614a6506c4ed06f0ec1e6ea076881492e8b4c
Guillermo Angulo <anyermo@gmail.com>
2020-12-05 18:30:54 -0300

    Add CardsList component

--- src/components/FormProfile/stories.tsx
+++ src/components/FormProfile/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import FormProfile from '.'
 
 export default {
-  title: 'FormProfile',
+  title: 'Profile/FormProfile',
   component: FormProfile
 } as Meta
 
