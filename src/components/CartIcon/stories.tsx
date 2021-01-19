6751ecde1b72544ff2aaf126fe470203a7101b39
Guillermo Angulo <anyermo@gmail.com>
2021-01-19 15:44:40 -0300

    Update CartList component to render an optional button

--- src/components/CartIcon/stories.tsx
+++ src/components/CartIcon/stories.tsx
@@ -1,7 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Button from 'components/Button'
 import CartIcon, { CartIconProps } from '.'
-import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 export default {
   title: 'CartIcon',
@@ -19,18 +17,3 @@ export const withItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
 withItems.args = {
   quantity: 1
 }
-
-export const withAnimation: Story<CartIconProps> = (args) => (
-  <div
-    style={{
-      display: 'flex',
-      width: '120px',
-      justifyContent: 'space-between'
-    }}
-  >
-    <CartIcon {...args} />{' '}
-    <Button size={'small'} icon={<AddShoppingCart />}>
-      Añadir
-    </Button>
-  </div>
-)
