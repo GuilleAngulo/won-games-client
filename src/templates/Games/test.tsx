c3ba3132f695d0d32343addb0017938fa502cf59
Guillermo Angulo <anyermo@gmail.com>
2021-02-26 19:18:25 -0300

    Update Show More and gamesConnection query

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -5,9 +5,20 @@ import { MockedProvider } from '@apollo/client/testing'
 import Games from '.'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import { gamesMock, fetchMoreMock } from './mocks'
+import { gamesMock, fetchMoreMock, noGamesMock } from './mocks'
 import userEvent from '@testing-library/user-event'
 import apolloCache from 'utils/apolloCache'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
 
 jest.mock('templates/Base', () => {
   return {
@@ -18,34 +29,25 @@ jest.mock('templates/Base', () => {
   }
 })
 
-jest.mock('components/ExploreSidebar', () => {
-  return {
-    __esModule: true,
-    default: function Mock({ children }: { children: React.ReactNode }) {
-      return <div data-testid="Mock ExploreSidebar">{children}</div>
-    }
-  }
-})
-
-// jest.mock('components/GameCard', () => {
+// jest.mock('components/ExploreSidebar', () => {
 //   return {
 //     __esModule: true,
-//     default: function Mock() {
-//       return <div data-testid="Mock GameCard"></div>
+//     default: function Mock({ children }: { children: React.ReactNode }) {
+//       return <div data-testid="Mock ExploreSidebar">{children}</div>
 //     }
 //   }
 // })
 
 describe('<Games />', () => {
-  it('should render loading when starting the template', () => {
-    renderWithTheme(
-      <MockedProvider mocks={[]} addTypename={false}>
-        <Games filterItems={filterItemsMock} />
-      </MockedProvider>
-    )
+  // it('should render loading when starting the template', () => {
+  //   renderWithTheme(
+  //     <MockedProvider mocks={[]} addTypename={false}>
+  //       <Games filterItems={filterItemsMock} />
+  //     </MockedProvider>
+  //   )
 
-    expect(screen.getByText(/loading.../i)).toBeInTheDocument()
-  })
+  //   expect(screen.getByText(/loading.../i)).toBeInTheDocument()
+  // })
 
   it('should render sections properly', async () => {
     renderWithTheme(
@@ -54,9 +56,9 @@ describe('<Games />', () => {
       </MockedProvider>
     )
 
-    expect(screen.getByText(/loading.../i)).toBeInTheDocument()
+    // expect(screen.getByText(/loading.../i)).toBeInTheDocument()
 
-    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+    expect(await screen.findByText(/price/i)).toBeInTheDocument()
 
     expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
@@ -64,6 +66,18 @@ describe('<Games />', () => {
 
     expect(
       await screen.findByRole('button', { name: /show more/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render empty when no games found', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[noGamesMock]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    expect(
+      await screen.findByText(/we didn't find any games with this filter/i)
     ).toBeInTheDocument()
   })
 
@@ -80,5 +94,21 @@ describe('<Games />', () => {
     userEvent.click(await screen.findByRole('button', { name: /show more/i }))
 
     expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
+  })
+
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
+    userEvent.click(await screen.findByLabelText(/low to high/i))
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/games',
+      query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
+    })
   })
 })
