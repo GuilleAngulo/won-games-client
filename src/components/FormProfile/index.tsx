983605c4a06eaae9fa929713db70de46a0ead1b8
Guillermo Angulo <anyermo@gmail.com>
2021-03-30 17:02:10 -0300

    Add resetValidate and forgotPassword validations

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -3,7 +3,12 @@ import Heading from 'components/Heading'
 import TextField from 'components/TextField'
 import * as S from './styles'
 
-const FormProfile = () => (
+export type FormProfileProps = {
+  username?: string
+  email?: string
+}
+
+const FormProfile = ({ email, username }: FormProfileProps) => (
   <>
     <Heading lineBottom color="black" size="small">
       My profile
@@ -11,17 +16,17 @@ const FormProfile = () => (
 
     <S.Form>
       <TextField
-        name="name"
-        placeholder="Name"
-        label="Name"
-        initialValue="John Doe"
+        name="username"
+        placeholder="Username"
+        label="Username"
+        initialValue={username}
       />
 
       <TextField
         name="email"
         type="email"
         placeholder="E-mail"
-        initialValue="johndoe@gmail.com"
+        initialValue={email}
         label="E-mail"
         disabled
       />
--- src/components/FormResetPassword/index.tsx
+++ src/components/FormResetPassword/index.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
-import { Email, ErrorOutline } from '@styled-icons/material-outlined'
+import { ErrorOutline } from '@styled-icons/material-outlined'
 import { signIn } from 'next-auth/client'
 
 import Button from 'components/Button'
-import TextField from 'components/TextField'
 import PasswordField from 'components/PasswordField'
 
-import * as S from './styles'
-import { FormLink, FormWrapper, FormLoading, FormError } from 'components/Form'
-import { FieldErrors, signInValidate } from 'utils/validations'
+import { FormWrapper, FormLoading, FormError } from 'components/Form'
+import { FieldErrors } from 'utils/validations'
 
-const FormSignIn = () => {
+const FormResetPassword = () => {
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ email: '', password: '' })
+  const [values, setValues] = useState({ password: '', confirm_password: '' })
   const [loading, setLoading] = useState(false)
   const routes = useRouter()
   const { push, query } = routes
@@ -28,7 +25,7 @@ const FormSignIn = () => {
     event.preventDefault()
     setLoading(true)
 
-    const errors = signInValidate(values)
+    const errors = {}
 
     if (Object.keys(errors).length) {
       setFieldError(errors)
@@ -60,36 +57,24 @@ const FormSignIn = () => {
         </FormError>
       )}
       <form onSubmit={handleSubmit}>
-        <TextField
-          name="email"
-          placeholder="Email"
-          type="email"
-          error={fieldError?.email}
-          onInputChange={(value) => handleInput('email', value)}
-          icon={<Email />}
-        />
         <PasswordField
           name="password"
           placeholder="Password"
           error={fieldError?.password}
-          onInputChange={(value) => handleInput('password', value)}
+          onInputChange={(v) => handleInput('password', v)}
         />
-        <Link href="/forgot-password" passHref>
-          <S.ForgotPassword>Forgot your password?</S.ForgotPassword>
-        </Link>
+        <PasswordField
+          name="confirm_password"
+          placeholder="Confirm password"
+          error={fieldError?.confirm_password}
+          onInputChange={(v) => handleInput('confirm_password', v)}
+        />
 
         <Button type="submit" size="large" fullWidth disabled={loading}>
-          {loading ? <FormLoading /> : <span>Sign in now</span>}
+          {loading ? <FormLoading /> : <span>Reset Password</span>}
         </Button>
-
-        <FormLink>
-          Don`t have an account?{' '}
-          <Link href="sign-up">
-            <a>Sign up</a>
-          </Link>
-        </FormLink>
       </form>
     </FormWrapper>
   )
 }
-export default FormSignIn
+export default FormResetPassword
