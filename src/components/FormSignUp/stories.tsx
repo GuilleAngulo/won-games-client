19ba28558f089b2a87b14cf7935cd4f7c5e87391
Guillermo Angulo <anyermo@gmail.com>
2021-04-05 10:09:13 -0300

    Fix SignUp story with ApolloProvider

--- src/components/FormSignUp/stories.tsx
+++ src/components/FormSignUp/stories.tsx
@@ -1,6 +1,8 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { withNextRouter } from 'storybook-addon-next-router'
+import { ApolloProvider } from '@apollo/client'
 import FormSignUp from '.'
+import { useApollo } from 'utils/apollo'
 
 export default {
   title: 'Form/FormSignUp',
@@ -8,8 +10,13 @@ export default {
   decorators: [withNextRouter]
 } as Meta
 
-export const Default: Story = () => (
-  <div style={{ width: 300, margin: 'auto' }}>
-    <FormSignUp />
-  </div>
-)
+export const Default: Story = () => {
+  const client = useApollo()
+  return (
+    <div style={{ width: 300, margin: 'auto' }}>
+      <ApolloProvider client={client}>
+        <FormSignUp />
+      </ApolloProvider>
+    </div>
+  )
+}
